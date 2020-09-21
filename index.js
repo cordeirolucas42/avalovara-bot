491e61efb19452c8e8ddca08fe605cece2e804e7
Lucas Cordeiro <cordeirolucas42@hotmail.com>
2020-09-21 20:33:29 -0300

    added postgresql database so the bot won't repeat quotes

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var Twit = require('twit')
 var EnvVar = require('dotenv')
 EnvVar.config();
 const fs = require('fs');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 var T = new Twit({
     consumer_key: process.env.CONSUMER_KEY,
@@ -10,48 +18,59 @@ var T = new Twit({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 })
 
-//post tweets periodicaly
-try {
-    var data = fs.readFileSync('source2.txt', 'utf8')
-    var quotes = data.split(/\r\n|\r|\n/)
-    var remove = []
-    for (var i in quotes){
-        if (quotes[i] === ""){
-            remove.push(i)
-        }
+client.connect();
+client.query("SELECT * FROM quotes ORDER BY random() limit 1;", (err,res) => {
+    if (err){
+        console.log(err);
     }
-    for (var i = remove.length-1; i > -1; i--){
-        quotes.splice(remove[i],1)
+    if (res.rows.length === 0){
+        console.log("ALREADY TWEETED ALL QUOTES, WILL START AGAIN");
+        client.query("INSERT INTO quotes SELECT * FROM tweeted; DELETE FROM tweeted;", (err,res) => {
+            client.query("SELECT * FROM quotes ORDER BY random() limit 1;", (err,res) => {
+                var quote = res.rows[0].quote;
+                console.log(quote);
+                T.post('statuses/update', { status: quote }, function(err, data, response) {
+                    console.log("avalovara quote tweeted successfuly at: " + data.created_at);
+                    console.log(data.text);
+                    client.query("DELETE FROM quotes WHERE quote=('" + quote + "'); INSERT INTO tweeted VALUES ('" + quote + "');", (err,res) => {
+                        client.end();
+                    });
+                });
+            });
+        });
+    } else {
+        console.log("NORMAL TWEET");
+        var quote = res.rows[0].quote;
+        console.log(quote);
+        T.post('statuses/update', { status: quote }, function(err, data, response) {
+            console.log("avalovara quote tweeted successfuly at: " + data.created_at);
+            console.log(data.text);
+            client.query("DELETE FROM quotes WHERE quote=('" + quote + "'); INSERT INTO tweeted VALUES ('" + quote + "');", (err,res) => {
+                client.end();
+            });
+        });
     }
-    console.log("Got " + quotes.length + " quotes sucessfully and will start tweeting periodicaly")
-} catch(e) {
-    console.log('Error:', e.stack)
-}
+});
 
-var quote = quotes[Math.floor(Math.random()*quotes.length)]
-// console.log("loop start")
-T.post('statuses/update', { status: quote }, function(err, data, response) {
-    console.log("avalovara quote tweeted successfuly at: " + data.created_at)
-    console.log(data.text)
-})
-
-//follow twitter account and retweet when the tweet involves some specific word
-// var users = ["721774492620582912"];
-// var stream = T.stream('statuses/filter', {follow: users});
-// stream.on('tweet', function (tweet) {
-//     if (users.indexOf(tweet.user.id_str) > -1) {
-//         console.log(tweet.user.name + ": " + tweet.text);
-//         if (tweet.text.includes("avalovara")){
-//             T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-//                 console.log(data)
-//             })
+// try {
+//     var data = fs.readFileSync('source2.txt', 'utf8')
+//     var quotes = data.split(/\r\n|\r|\n/)
+//     var remove = []
+//     for (var i in quotes){
+//         if (quotes[i] === ""){
+//             remove.push(i)
 //         }
 //     }
-// })
-
-//search tweets in the last 7 days for specific words and users
-// T.get('search/tweets', { q: 'avalovara from:erosinbetween', count: 100}, function(err, data, response) {
-//     for (var i in data.statuses){
-//         console.log(data.statuses[i].text)
+//     for (var i = remove.length-1; i > -1; i--){
+//         quotes.splice(remove[i],1)
 //     }
+//     console.log("Got " + quotes.length + " quotes sucessfully and will start tweeting periodicaly")
+// } catch(e) {
+//     console.log('Error:', e.stack)
+// }
+
+// var quote = quotes[Math.floor(Math.random()*quotes.length)]
+// T.post('statuses/update', { status: quote }, function(err, data, response) {
+//     console.log("avalovara quote tweeted successfuly at: " + data.created_at)
+//     console.log(data.text)
 // })
